51e678ee6ba221d7741aae8abdffd471cce05ff7
donaldwong <donaldkien12@gmail.com>
2023-04-15 18:10:16 +0800

    light theme dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import Home from "pages/index";
+
+const theme = (lightTheme: boolean) => {
+  if (lightTheme) return "light-theme";
+  else return "dark-theme";
+};
+
 function App() {
-  return <div>hello</div>;
+  const [lightTheme, setLightTheme] = useState<boolean>(true);
+
+  return (
+    <div className={`app ${theme(lightTheme) || ""}`}>
+      <button onClick={() => setLightTheme((prev) => !prev)}>change</button>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.FC<{}> = () => {
+  return (
+    <div className="page-home">
+      <div className="home"></div>
+    </div>
+  );
+};
+
+export default Home;
